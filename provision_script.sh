109db182046d29be2d5c19b09f8c1a46704de2ee
Matthew Alston <imakelists@gmx.us>
2025-06-27 22:41:27 -0700

    update to setting desktop

--- provision_script.sh
+++ provision_script.sh
@@ -13,6 +13,6 @@ sudo add-apt-repository -y ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install -y obs-studio
 
-sudo bash -c 'wget -O /tmp/desktop.png https://raw.githubusercontent.com/sterlingalston/sbapp-78102/refs/heads/main/desktop.png && find /usr/share/wallpapers/Next/contents/images -type f -exec cp /tmp/desktop.png {}; sudo -u $SUDO_USER gsettings set org.gnome.desktop.background picture-uri "file:///tmp/desktop.png" && sudo -u $SUDO_USER gsettings set org.gnome.desktop.background picture-uri-dark "file:///tmp/desktop.png"'
+wget -qO- https://raw.githubusercontent.com/sterlingalston/sbapp-78102/refs/heads/main/set_wallpaper.sh | bash
 
 obs
