a4fb5f30ba74a89c42c1f6f053d483d791766d5b
sterlingalston <sterlingalston@gmail.com>
2025-07-27 00:25:34 -0700

    Update provision_script.sh

--- provision_script.sh
+++ provision_script.sh
@@ -16,5 +16,3 @@ sudo apt install -y obs-studio
 obs
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'panels().forEach(function(panel) { panel.height = 0; })'
 sudo bash -c 'wget -O /tmp/desktop.png https://raw.githubusercontent.com/sterlingalston/sbapp-78102/refs/heads/main/desktop.png && find /usr/share/wallpapers/Next/contents/images -type f -exec cp /tmp/desktop.png {} \; && sudo -u $SUDO_USER gsettings set org.gnome.desktop.background picture-uri "file:///tmp/desktop.png" && sudo -u $SUDO_USER gsettings set org.gnome.desktop.background picture-uri-dark "file:///tmp/desktop.png"'
-
-sudo bash -c 'wget -O /tmp/server_cron.py https://raw.githubusercontent.com/sterlingalston/sbapp-78102/refs/heads/main/server_cron.py && chmod +x /tmp/server_cron.py && service cron start && (crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 /tmp/server_cron.py"; echo "* * * * * sleep 45 && /usr/bin/python3 /tmp/server_cron.py") | crontab -'
